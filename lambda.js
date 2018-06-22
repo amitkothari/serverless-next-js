9dbdef0dc56d1a924f3f4d22f9fb558da2731c48
Amit Kothari <amit@amitkothari.com>
2018-06-23 09:52:15 +1000

    lambda with aws-serverless-express

--- lambda.js
+++ lambda.js
@@ -1,9 +1,21 @@
-const serverless = require("serverless-http");
 const { app, server } = require("./server");
+const awsServerlessExpress = require("aws-serverless-express");
 
-exports.handler = (event, context, callback) => {
+exports.handler = (event, context) => {
   app.prepare().then(() => {
-    const handler = serverless(server);
-    return handler(event, context, callback);
+    return awsServerlessExpress.proxy(
+      awsServerlessExpress.createServer(server),
+      event,
+      context
+    );
   });
 };
+
+// const serverless = require("serverless-http");
+
+// exports.handler = (event, context, callback) => {
+//   app.prepare().then(() => {
+//     const handler = serverless(server);
+//     return handler(event, context, callback);
+//   });
+// };
